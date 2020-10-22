587a4cb29110d54e31f42aedbbf6174cba500d04
Josh Ghent <me@joshghent.com>
2020-10-22 12:54:18 +0100

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,12 +1,16 @@
 echo "# Removing"
 
+if [[ $(id -u) -ne 0 ]];
+        then echo "Please re-run this script as root";
+        exit 1;
+fi
+
 wg-quick down wg0
 systemctl stop wg-quick@wg0
 systemctl disable wg-quick@wg0
 
-yes | apt autoremove wireguard wireguard-dkms wireguard-tools
-#yes | apt autoremove software-properties-common
-yes | apt update
+yes | yum remove wireguard wireguard-dkms wireguard-tools
+yes | yum -y update
 
 rm -rf /etc/wireguard
 
